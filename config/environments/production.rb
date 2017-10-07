4564121a4d48e85f4aa83a1c2e966ed647212179
v3gaj <juanvegaquesada@gmail.com>
2017-10-07 11:37:55 -0600

    Style

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,12 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
+
+  config.assets.precompile =  ['*.js', '*.css', '*.scss', '*min.css', '*.ttf', '*.woff', '*.woff2']
+  
+  
+  config.public_file_server.enabled = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
